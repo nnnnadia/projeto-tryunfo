bcb2f3bbc4f0f7d7bbc1ca31194059ee3963adf3
nnnnadia <tristaonadia@gmail.com>
2022-05-14 18:02:09 -0300

    Adiciona estilo às cartas.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import '../style/Card.css';
 
 export default class Card extends Component {
+  countStars = (cardRare) => {
+    if (cardRare === 'muito raro') return '★★★';
+    if (cardRare === 'raro') return '★★';
+    return '★';
+  }
+
   render() {
-    const {
+    const { props: {
       cardName,
       cardDescription,
       cardAttr1,
@@ -12,18 +19,53 @@ export default class Card extends Component {
       cardImage,
       cardRare,
       cardTrunfo,
-    } = this.props;
+    }, countStars } = this;
     return (
-      <>
-        <h2 data-testid="name-card">{ cardName }</h2>
-        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
-        <p data-testid="description-card">{ cardDescription }</p>
-        <span data-testid="attr1-card">{ cardAttr1 }</span>
-        <span data-testid="attr2-card">{ cardAttr2 }</span>
-        <span data-testid="attr3-card">{ cardAttr3 }</span>
-        <p data-testid="rare-card">{ cardRare }</p>
+      <div className="card">
+        <div className="card-content">
+          <div className="card-title">
+            <span
+              className="card-stars"
+              data-testid="rare-card"
+            >
+              { countStars(cardRare) }
+            </span>
+            <h2
+              className="card-name"
+              data-testid="name-card"
+            >
+              { cardName }
+            </h2>
+          </div>
+          <img
+            className="card-image"
+            data-testid="image-card"
+            src={ cardImage }
+            alt={ cardName }
+          />
+          <p
+            className="card-description"
+            data-testid="description-card"
+          >
+            { cardDescription }
+          </p>
+          <div className="attr-content">
+            <div className="attr-container">
+              <p>Atributo 1</p>
+              <span data-testid="attr1-card">{ cardAttr1 }</span>
+            </div>
+            <div className="attr-container">
+              <p>Atributo 2</p>
+              <span data-testid="attr2-card">{ cardAttr2 }</span>
+            </div>
+            <div className="attr-container">
+              <p>Atributo 3</p>
+              <span data-testid="attr3-card">{ cardAttr3 }</span>
+            </div>
+          </div>
+        </div>
         { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>}
-      </>
+      </div>
     );
   }
 }
