9603876f16ecf30b133e4c0a0eb5c3fc4e0f5885
nnnnadia <tristaonadia@gmail.com>
2022-05-16 19:01:19 -0300

    Requisito 5: O botão de salvar é ativado/desativado de acordo com a validação do formulário.

--- src/App.js
+++ src/App.js
@@ -6,26 +6,69 @@ class App extends React.Component {
   state = {
     cardName: '',
     cardDescription: '',
-    cardAttr1: '',
-    cardAttr2: '',
-    cardAttr3: '',
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
     cardImage: '',
     cardRare: '',
     cardTrunfo: false,
     hasTrunfo: false,
-    isSaveButtonDisabled: false,
+    isSaveButtonDisabled: true,
   };
+
+  validateText = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+    } = this.state;
+    if (cardName.length > 0
+      && cardDescription.length > 0
+      && cardImage.length > 0) return true;
+    return false;
+  }
+
+  validateAttributes = () => {
+    const {
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+    const attr1 = +cardAttr1;
+    const attr2 = +cardAttr2;
+    const attr3 = +cardAttr3;
+    const minAttr = 0;
+    const maxAttr = 90;
+    const maxSum = 210;
+    if (Math.min(attr1, attr2, attr3) >= minAttr
+      && Math.max(attr1, attr2, attr3) <= maxAttr
+      && (attr1 + attr2 + attr3) <= maxSum) return true;
+    return false;
+  }
+
+  validateInputs = () => {
+    const {
+      validateText,
+      validateAttributes,
+    } = this;
+    if ((validateText()) && (validateAttributes())) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  }
 
   onInputChange = ({ target }) => {
     const { name, type } = target;
     const value = type === 'checkbox'
       ? target.checked
       : target.value;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value },
+      () => this.validateInputs());
   }
 
   onSaveButtonClick = () => {
-
+    console.log('oi');
   }
 
   render() {
